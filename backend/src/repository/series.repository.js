2f8fbd9c4f76fc9e6221b49f1b4442de46c01f90
praful-koli <prafulkoli852@gmail.com>
2026-06-14 13:56:25 +0530

    feat: implement SeriesRepository for managing series data

---
+++ backend/src/repository/series.repository.js
@@ -0,0 +1,47 @@
+import { seriesModel } from "../models/series.model";
+
+class SeriesRepositroy {
+  async create(data) {
+    return await seriesModel.create(data);
+  }
+  async findAll() {
+    return await seriesModel.find({ isDeleted: false }).sort({ createdAt: -1 });
+  }
+  async findById(id) {
+    return await seriesModel.findOne({
+      _id: id,
+      isDeleted: false,
+    });
+  }
+  async findBySeason(season) {
+    return await seriesModel.findOne({
+      season,
+      isDeleted: false,
+    });
+  }
+
+  async updateById(id, data) {
+    return await seriesModel.findOneAndUpdate(
+      { _id: id, isDeleted: false },
+      data,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+  }
+
+  async softDeleteById(id, userId) {
+    return await seriesModel.findOneAndUpdate(
+      { _id: id, isDeleted: false },
+      {
+        isDeleted: true,
+        updatedBy: userId,
+      },
+      { new: true },
+    );
+  }
+}
+
+
+export const seriesRepo = new SeriesRepositroy()
