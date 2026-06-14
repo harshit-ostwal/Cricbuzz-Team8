015e17b4df1c0784131385480639bb11c7aa7d60
praful-koli <prafulkoli852@gmail.com>
2026-06-14 14:39:50 +0530

    fix: update reference in series model for createdBy and updatedBy fields

--- backend/src/models/series.model.js
+++ backend/src/models/series.model.js
@@ -11,8 +11,8 @@ const seriesSchema = new Schema(
     },
     logo: String,
     isDeleted: { type: Boolean, default: false },
-    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
-    updatedBy: { type: Schema.Types.ObjectId, ref: "User" },
+    createdBy: { type: Schema.Types.ObjectId, ref: "users" },
+    updatedBy: { type: Schema.Types.ObjectId, ref: "users" },
   },
   { timestamps: true },
 );
